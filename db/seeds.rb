1f7afaffe9ff044a77d074f4b254ec234514e9da
Miguel Mendoza <info@miguelmendoza.dj>
2016-09-13 12:08:45 +0200

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if Book.all.size == 0
+  title_sources = %w{Faker::Hipster.sentences(1)[0] Faker::ChuckNorris.fact Faker::Book.title Faker::Hacker.say_something_smart Faker::Space.star}
+  author_sources = %w{Faker::Superhero.name Faker::StarWars.character Faker::GameOfThrones.character Faker::Book.author Faker::Pokemon.name}
+  1000.times do
+    ti = rand 5
+    ai = rand 5
+    Book.create!(title: eval(title_sources[ti]), author: eval(author_sources[ai]))
+  end
+end
